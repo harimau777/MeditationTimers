0cfeb04c18044f8f1e295b115c560094e6f5a89a
David Moneysmith <moneysmith777@gmail.com>
2017-02-22 06:39:34 -0800

    Enabled compression on the server.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 
 const server = new express();
 
-server.use(express.static(__dirname + '/../client'));
+server.use(compression());  //Enable compression
+server.use(express.static(__dirname + '/../client')); //Serve static content
 
 server.listen(8080, () => console.log('Server is now listening on port 8080'));
