f9cce7789529177f2d425ff332fc59ed434683b2
David Moneysmith <moneysmith777@gmail.com>
2017-02-27 14:29:24 -0800

    Layout of Timer component now more consistent as # of digits in duration changes

--- client/app/timer.jsx
+++ client/app/timer.jsx
@@ -3,7 +3,7 @@ const Timer = ({index, duration, increment, decrement, remove}) => (
     <h1>Timer {index + 1}</h1>
     <div className="durationControl">
       <span className='button buttonIncrement' onClick={increment}>Increment</span>
-      <span>{duration} minute{duration !== 1 ? 's' : ''}</span>
+      <span className='timerDuration'>{duration} minute{duration !== 1 ? 's' : ''}</span>
       <span className='button buttonDecrement' onClick={() => decrement(index)}>Decrement</span>
     </div>
     <span className="button buttonRemoveTimer" onClick={() => remove(index)}>Remove Timer</span>
